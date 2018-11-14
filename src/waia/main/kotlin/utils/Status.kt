3370f96a1b7ed81706cdd60feb9e98a574569b7e
Ariel Guzman <ariel.guzman01@gmail.com>
2018-11-14 18:33:22 -0400

    deleted all files

--- src/waia/main/kotlin/utils/Status.kt
+++
@@ -1,3 +0,0 @@
-package waia.main.kotlin.utils
-
-data class Status(val success: Boolean = true, val message: String = "Success")
--- src/waia/main/kotlin/utils/User.kt
+++
@@ -1,8 +0,0 @@
-package waia.main.kotlin.utils
-
-import mongo
-import org.litote.kmongo.findOne
-import org.litote.kmongo.getCollection
-import waia.main.kotlin.user.User
-
-fun currentUser(accessToken: String): User? = mongo().database.getCollection<User>().findOne()
