d754669989596743b3f3ebc0a0727cfe011a7040
Ariel Guzman <ariel.guzman01@gmail.com>
2018-11-16 14:33:38 -0400

    Added token checks against existing users.

--- app/actions/Authorization.scala
+++ app/actions/Authorization.scala
@@ -1,5 +1,8 @@
 package actions
 
+import java.util.Calendar
+
+import models.AccessTokens
 import play.api.http.HeaderNames
 import play.api.libs.json.Json
 import play.api.mvc.{Action, BodyParser, Request, Result}
@@ -7,18 +10,25 @@ import play.api.mvc.Results._
 
 import scala.concurrent.{ExecutionContext, Future}
 
-case class Authorization[A](action: Action[A]) extends Action[A]{
+case class Authorization[A](action: Action[A]) extends Action[A] {
 
   override def apply(request: Request[A]): Future[Result] = {
     request.headers.get(HeaderNames.AUTHORIZATION) match {
       case None =>
         Future.successful(
-          Forbidden(Json.toJson(utils.Status(success = false, message = "Unauthorized")))
+          Forbidden(Json.toJson(utils.Status(success = false, message = "Forbidden")))
         )
-      case Some(value) =>  action(request)
-
+      case Some(value) =>
+        val token = AccessTokens.findByToken(value)
+        val now = Calendar.getInstance.getTimeInMillis
+        if (token != null && token.tls > now) {
+          action(request)
+        } else {
+          Future.successful(
+            Unauthorized(Json.toJson(utils.Status(success = false, message = "Unauthorized")))
+          )
+        }
     }
-
   }
 
   override def parser: BodyParser[A] = action.parser
